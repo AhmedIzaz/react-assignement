ea3b31c2e8e140a247355cf12ec5a50664d0d2bf
Ahmed Izaz Bhuiyan <ahmedasif59532@gmail.com>
2022-02-14 19:14:33 +0600

    final added

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Loader from "../loaders/Loader";
-
+import "./productStyle.css";
 function Product({ product }) {
   const [theProduct, setProduct] = useState(null);
   useEffect(() => setTimeout(() => setProduct(product), 1000), []);
@@ -9,7 +9,7 @@ function Product({ product }) {
       {!theProduct ? (
         <Loader />
       ) : (
-        <div>
+        <div className="product-container">
           <h3>{theProduct.name}</h3>
           <p>{theProduct.description}</p>
           <small>
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-
+import "./productListStyle.css";
 function ProductList({ product_list, onProductClick }) {
   return (
     <div>
       {product_list.map((product) => (
         <div onClick={() => onProductClick(product)}>
-          <h1>{product.name}</h1>
+          <h1 className="product-name">{product.name}</h1>
         </div>
       ))}
     </div>
