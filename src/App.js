6aa21d6873ff4f9a3f9952e54291310912f3883d
Ahmed Izaz Bhuiyan <ahmedasif59532@gmail.com>
2022-02-20 19:27:36 +0600

    added

--- src/App.js
+++ src/App.js
@@ -1,106 +1,80 @@
-import Loader from "./loaders/Loader";
-import React, { useState, useEffect } from "react";
+import { Button, Grid, TextField, Typography } from "@material-ui/core";
+import React, { useState } from "react";
 import "./App.css";
-import { Grid, IconButton } from "@material-ui/core";
-import { List_Item } from "./styled_component";
-import { Facebook, GitHub, Instagram, LinkedIn } from "@material-ui/icons";
+const App = () => {
+  const [data, setData] = useState("");
+  const [tasks, setTasks] = useState([]);
 
-function App() {
-  const [show, setShow] = useState(false);
-  useEffect(() => setTimeout(() => setShow(!show), 1000), []);
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (!data) return alert("Please Add Item First!");
+    let tasks_copy = [...tasks];
+    tasks_copy.push(data);
+    setTasks(tasks_copy);
+    setData("");
+  };
+
+  const edit_task = (index) => {};
+
+  const delete_task = (index) => {};
   return (
-    <>
-      {!show ? (
-        <Loader />
-      ) : (
-        <div className="container">
-          <Grid container spacing={3}>
-            <Grid item xs={12} sm={8} md={6}>
-              <div className="picture-container">
-                <img
-                  className="picture"
-                  src="https://media-exp1.licdn.com/dms/image/D5603AQGwB1lM13mHng/profile-displayphoto-shrink_800_800/0/1638289276949?e=1650499200&v=beta&t=PCuikM0DDIOqepXVrONKZkH6oWR0g-AiEm9z_8A8Ppw"
-                  alt="my_picture"
-                />
+    <div className="container">
+      <h2>Todo App</h2>
+      <div className="form-container">
+        <form className="form" onSubmit={onFormSubmit}>
+          <TextField
+            label="task"
+            value={data}
+            onChange={(e) => setData(e.target.value)}
+            type="text"
+            variant="outlined"
+            fullWidth
+            size="small"
+          />
+          <Button
+            variant="outlined"
+            color="secondary"
+            onClick={() => setData("")}
+          >
+            Clear
+          </Button>
+          <Button variant="contained" color="primary" type="submit">
+            Add
+          </Button>
+        </form>
+      </div>
+      <div className="tasks-list">
+        {tasks.length == 0 ? (
+          <Typography align="center" variant="h3" color="textSecondary">
+            No Task Add Yet!
+          </Typography>
+        ) : (
+          tasks.map((task, index) => (
+            <div className="task-container">
+              <div className="task-info">
+                <p>{task}</p>
               </div>
-            </Grid>
-            <Grid item xs={12} sm={4} md={6} spacing={2}>
-              <div className="about-skills">
-                <div className="about">
-                  <h3>Ahmed Izaz Bhuiyan</h3>
-                  <p>
-                    Working as a Full Stack developer in Javascript specially
-                    ReactJS, NodeJS, MySQL. MongoDB and Django framework of
-                    Python about 4 year
-                  </p>
-                </div>
-                <div className="skills">
-                  <h3>Skills</h3>
-                  <ul>
-                    <List_Item>Javascript</List_Item>
-                    <List_Item>NodeJS</List_Item>
-                    <List_Item>React JS</List_Item>
-                    <List_Item>Next JS</List_Item>
-                    <List_Item>Express JS</List_Item>
-                    <List_Item>Typescript</List_Item>
-                  </ul>
-                  <ul>
-                    <List_Item>Bootstrap</List_Item>
-                    <List_Item>Material UI</List_Item>
-                    <List_Item>Tailwind CSS</List_Item>
-                    <List_Item>Python</List_Item>
-                    <List_Item>Django</List_Item>
-                    <List_Item>REST Framework</List_Item>
-                  </ul>
-                </div>
+              <div className="task-actions">
+                <Button variant="outlined" color="default">
+                  Edit
+                </Button>
+                <Button
+                  onClick={() =>
+                    setTasks([...tasks[index - 1], ...tasks[index + 1]])
+                  }
+                  variant="contained"
+                  color="secondary"
+                >
+                  Delete
+                </Button>
               </div>
-            </Grid>
-          </Grid>
-          <Grid container spacing={3}>
-            <Grid item xs={12} sm={8} md={6}>
-              <div className="experience">
-                <h3>Experience</h3>
-                <p>
-                  I have worked with a team called Proton Seed since 2018.. Have
-                  perticipated at some projects about Employee Information
-                  Management, Employement ,Forum etc. Also I developed myself a
-                  Resturant Web Application using MERN Stack
-                </p>
-              </div>
-            </Grid>
-            <Grid item xs={12} sm={4} md={6}>
-              <div className="contact">
-                <h3>Contact</h3>
-                <p>Mobile : 01839465030</p>
-                <p>Email : asif32803@gmail.com</p>
-                <div className="icons">
-                  <IconButton>
-                    <Facebook />
-                  </IconButton>
-                  <IconButton>
-                    <Instagram />
-                  </IconButton>
-                  <a
-                    target="_blank"
-                    href="https://www.linkedin.com/in/ahmed-izaz-bhuiyan-1289a1172/"
-                  >
-                    <IconButton>
-                      <LinkedIn />
-                    </IconButton>
-                  </a>
-                  <a target="_blank" href="https://github.com/ahmed-izaz53">
-                    <IconButton>
-                      <GitHub />
-                    </IconButton>
-                  </a>
-                </div>
-              </div>
-            </Grid>
-          </Grid>
-        </div>
-      )}
-    </>
+            </div>
+          ))
+        )}
+      </div>
+    </div>
   );
-}
+};
 
 export default App;
